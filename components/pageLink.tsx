d0ab68f6fc7266814db69ab2098af547956ae2cc
Jonas Hamann <jonashamann@icloud.com>
2024-12-07 16:03:57 +0100

    add linking to task routes to default route

---
+++ components/pageLink.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Card } from "./ui/card";
+
+interface PageLinkProps {
+    href: string;
+    name: string;
+    children?: React.ReactNode;
+}
+
+export function PageLink({href, name, children}: PageLinkProps) {
+  return (
+    <Link href={href}>
+        <Card className="w-1/3 h-40 flex justify-center items-center hover:bg-slate-200 hover:scale-105 transition-all" >
+            <div className="flex flex-col justify-center items-center">
+                {children}
+                <h2 className="text-xl font-bold">{name}</h2>   
+            </div>
+        </Card>
+        
+    </Link>
+  )
+}
