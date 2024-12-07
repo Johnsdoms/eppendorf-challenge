e105622865b8686bab5276da6007bb1f9e50b7b5
Jonas Hamann <jonashamann@icloud.com>
2024-12-07 18:51:12 +0100

    update dependencies and update eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+    react: true,
+    stylistic: true,
+    stylistic: {
+        indent: 2,
+        quotes: 'double',
+    },
+})
